7ee82364fe73139c571ebd964a9ac95205bb60a5
semih612 <semih.zaman@epfl.ch>
2025-08-29 14:43:00 +0300

    -

--- calculate_A_B_C.m
+++ calculate_A_B_C.m
@@ -28,6 +28,11 @@ function [A, B, C] = calculate_A_B_C(x, u, Ntraj, Obs_state, n, m)
 % for later use.
 
 
+% Dimensions
+nObs_state = size(Obs_state(zeros(n,1)));
+Nlift = nObs_state;
+nZ = Nlift(1);   % lifted state dimension
+
 X = reshape(x(:, 1:Ntraj, :), n, []);
 Y = reshape(x(:, 2:Ntraj+1, :), n, []);
 U = reshape(u(:, 1:Ntraj, :), m, []);
@@ -38,13 +43,18 @@ for i = 1:size(X, 2)
     X_lift(:, i) = Obs_state(X(:, i));
     Y_lift(:, i) = Obs_state(Y(:, i));
 end
+
 % Compute A and B
 Z = [X_lift; U]; % Stack lifted states and inputs
-
 %lambda = 1e-4;  % Tune this based on scale of Y_lift
-lambda = 1e-5 * norm(Z * Z', 'fro');
-AB = Y_lift * Z' / (Z * Z' + lambda * eye(size(Z,1)));
+%lambda = 1e-5 * norm(Z * Z', 'fro');
+%AB = Y_lift * Z' / (Z * Z' + lambda * eye(size(Z,1)));
 AB = Y_lift * pinv(Z);
+
+% Compute A and B, only first four states
+AB_temp = Y_lift(1:4,:) * pinv(Z);
+C_temp = [eye(4), zeros(4,nZ-4)];
+AB = pinv(C_temp) * AB_temp;
 
 
 A = AB(:, 1:size(X_lift, 1));
